63831c61e97ee27b2c3e11681c8648609724a0cb
Jorjo <gorgiladze888@gmail.com>
2024-02-19 01:54:33 +0400

    fix bug

--- src/components/calendar/Canvase.jsx
+++ src/components/calendar/Canvase.jsx
@@ -171,7 +171,8 @@ function Canvase({ eventsWithDateTime, getBookingFc, csrf }) {
 											</div>
 											<div className='col-6 text-center'>
 												<p>
-													{booking.date} <span>{booking.time.slice(0, 5)}</span>
+													{booking.date}{' '}
+													<span>{booking?.time.slice(0, 5)}</span>
 												</p>
 												<p>{booking.customer_phone}</p>
 											</div>
